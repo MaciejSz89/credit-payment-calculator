bb013acf85fbd4ab7842cbdbbafa62e919dd9b48
Maciej Szafranski <maciej.szafranski1@gmail.com>
2022-04-17 22:59:45 +0200

    2022-04-17

---
+++ CreditPaymentCalculator/Models/Loan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditPaymentCalculator.Models
+{
+    public class Loan
+    {
+        public decimal Balance { get; set; }
+        public decimal InterestRate { get; set; }
+        public DateTime EndDate { get; set; }
+        public LoanInstallmentType InstallmentType { get; set; }
+        public decimal NextInstallment { get; set; }
+
+    }
+}
